95323f1b9e16cbd6a4173e1557f5cdf385a465dc
William <102698609+willydibe24@users.noreply.github.com>
2023-04-20 11:00:45 +0200

    Add files via upload

---
+++ CLASSI Studente/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASSI_Studente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int v, last, nV, option;
+            double media;
+
+            Studente student = new Studente();
+            Console.WriteLine("Inserire il nome dello studente: ");
+            student.name = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("Inserire il numero di voti di " + student.name + ": ");
+                nV = int.Parse(Console.ReadLine());
+            } while (nV <= 0);
+            
+            for (int i = 0; i < nV; i++)
+            {
+                Console.WriteLine("Inserire il " +(i+1)+ "° voto: ");
+                student.Vote = int.Parse(Console.ReadLine());
+            }
+            do
+            {
+                Console.WriteLine("\nMENU\n1)Visualizza la media\n2)Visualizza l'ultimo voto\n3)Aggiungi un voto\n0)Esci");
+                option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        media = student.Media(nV);
+                        Console.WriteLine("Media dei voti: " + media);
+                        break;
+
+                    case 2:
+                        last = student.GetLast;
+                        Console.WriteLine("Ultimo voto: " + student.GetLast);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Aggiungi un voto: ");
+                        student.Vote = int.Parse(Console.ReadLine());
+                        nV++;
+                        break;
+
+                    case 0:
+                        Console.WriteLine("Il programma verrà chiuso");
+                        break;
+                }
+            } while (option != 0);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CLASSI Studente/Studente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASSI_Studente
+{
+    internal class Studente
+    {
+        private string sName;
+        private int vote;
+        private int sum;
+
+        public string name
+        {
+            get { return sName; }
+            set { sName = value; }
+        }
+        public int Vote
+        {
+            get { return vote; }
+            set 
+            { 
+                vote = value;
+                sum = sum + vote;
+            }
+        }
+
+        public double Media (int nVotes)
+        {
+            return Math.Round((double)(sum) / nVotes , 2); 
+        }
+        public int GetLast
+        {
+            get { return vote; }
+        }
+    }
+}
