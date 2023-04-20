214234300386b47d0013a01b96e2713bdbb20055
William <102698609+willydibe24@users.noreply.github.com>
2023-04-20 10:29:38 +0200

    Add files via upload

---
+++ CLASSI Conto corrente/BankAccount.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASSI_Conto_corrente
+{
+    internal class BankAccount
+    {
+        private string AccountHolder; //correntista
+        private string AccountNumber =  "abc123"; //numeroconto
+        private double balance; //saldo
+
+        public BankAccount (string AccountHolder) //costruttore
+        {
+            this.AccountHolder = AccountHolder;
+            balance = 0;
+        }
+
+        //metodi
+        public string GetAccountHolder() 
+        {
+            return AccountHolder;
+        }
+
+        //proprietà
+        public string GetAccountNumber
+        {
+            get { return AccountNumber; }
+        }
+
+        public double GetSetBalance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+        public int Withdrawal (double withdrawal)
+        {
+            if (balance > 0 && balance < withdrawal)
+            {
+                balance = (double) balance - withdrawal;
+                return 1;
+            }
+            else
+                return 0;
+        }
+
+        public bool Deposit (double deposit)
+        {
+            if (deposit > 0)
+            {
+                balance = balance + deposit;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        //autenticazione
+        public bool Authentication (string AcconutHolder, string AccountNumber)
+        {
+            if (this.AccountHolder == AcconutHolder && this.AccountNumber == AccountNumber)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ CLASSI Conto corrente/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASSI_Conto_corrente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string num;
+            bool r;
+            
+            Console.WriteLine("GESTIONE CONTO CORRENTE\n");
+            Console.WriteLine("Inserire il nome del correntista: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Inserire il numero di conto di" + name + ": ");
+            num = Console.ReadLine();
+
+            BankAccount BAccount = new BankAccount(name);
+
+            r = BAccount.Authentication(name, num);
+            if (r == true)
+                Console.WriteLine("Autenticazione riuscita!");
+            else
+                Console.WriteLine("Autenticazione fallita! Il programma verrà chiuso");
+
+            Console.ReadKey();
+        }
+    }
+}
