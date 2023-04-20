193e374c36adbc9d4d11cea4a0f33a97746f89e9
William <102698609+willydibe24@users.noreply.github.com>
2023-04-20 10:32:28 +0200

    Add files via upload

---
+++ CLASSI Course/Course.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASSI_Course
+{
+    internal class Course
+    {
+        private string CourseName;
+        private string InstructorName;
+        private int Duration;
+        private List<string> names = new List<string>();
+
+        public Course (string CourseName, string InstructorName, int Duration)
+        {
+            this.CourseName = CourseName;
+            this.InstructorName = InstructorName;
+            this.Duration = Duration;
+        }
+
+        public void AddSubscriber(string Subscriber)
+        {
+            names.Add(Subscriber);
+        }
+
+        public bool RemoveSubsciber(string Subscriber)
+        {
+            return names.Remove(Subscriber);
+        }
+
+        public int NumberOfSubscribers()
+        {
+            return names.Count;
+        }
+
+        public string GetSubscribers(int i)
+        {
+            return names[i];
+        }
+
+        public int Compare(string CourseName, int i)
+        {
+            int found = -1;
+            if (this.CourseName == CourseName)
+            {
+                found = i;
+            }
+            return found;
+        }
+    }
+}
