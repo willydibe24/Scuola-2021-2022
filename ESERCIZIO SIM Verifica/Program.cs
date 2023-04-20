9bd31cd38ab93ea6896a8dc54ba38e78b485b52c
William <102698609+willydibe24@users.noreply.github.com>
2023-04-20 11:08:34 +0200

    Add files via upload

---
+++ ESERCIZIO SIM Verifica/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESERCIZIO_SIM_Verifica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABILI CLASSE SIM
+            string phoneNumber;
+            string provider;
+            double credit;
+
+            // ALTRE VARIABILI
+            int option;
+
+
+            Console.WriteLine("GESTIONE SIM\n-----------------");
+            Console.WriteLine("\nInserire il numero di telefono: ");
+            phoneNumber = Console.ReadLine();
+            Console.WriteLine("\nInserire l'operatore telefonico: ");
+            provider = Console.ReadLine();
+            Console.WriteLine("\nInserire il credito presente: ");
+            credit = double.Parse(Console.ReadLine());
+
+            Sim sim = new Sim(phoneNumber, provider, credit);
+
+            do
+            {
+                Console.WriteLine("\nMENU"+
+                    "\n1)Effettua una telefonata" +
+                    "\n2)Visualizza l'importo totale delle telefonate fatte" +
+                    "\n3)Visualizza il tempo medio di chiamata" +
+                    "\n4)Informazioni sim" +
+                    "\n5)Ricarica la sim" +
+                    "\n0)Esci");
+
+
+                option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("\nInserire la durata della chiamata (in secondi): ");
+                        sim.timeSpent = double.Parse(Console.ReadLine());
+                        Console.WriteLine(sim.AddPhoneCall());
+                        break;
+
+                    case 0:
+                        Console.WriteLine("\nIl programma verrà chiuso");
+                        break;
+
+                    default:
+                        Console.WriteLine("\nErrore di digitazione, riprova");
+                        break;
+
+                }
+            } while (option != 0);
+
+        }
+    }
+}
---
+++ ESERCIZIO SIM Verifica/Sim.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESERCIZIO_SIM_Verifica
+{
+    internal class Sim
+    {
+        protected string phoneNumber;
+        protected string phoneCompany;
+        protected double credit;
+        protected int phoneCalls { get; set; } = 0;
+        protected int duration { get; set; } = 0;
+
+
+        public Sim (string phoneNumber, string phoneCompany, double credit)
+        {
+            this.phoneNumber = phoneNumber;
+            this.phoneCompany = phoneCompany;
+            this.credit = credit;
+        }
+
+        public string NewPhoneCall(int duration)
+        {
+            if (credit - 0.20 - ((duration / 30) * 0.30) <= 0)
+                return "\nCredito residuo insufficiente";
+
+            credit -= 0.20 - ((duration / 30) * 0.30);
+            this.duration += duration;
+            phoneCalls++;
+
+            return "\nChiamata effettuata correttamente";
+        }
+
+        public double TrafficImport()
+        {
+            return phoneCalls * 0.20 + ((duration / 30) * 0.30);
+        }
+    }
+}
